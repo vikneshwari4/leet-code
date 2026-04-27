b0015dd5353519aa00f8df2677f2f56880df0a1b
Vikneshwari <vikneshwari.p945@gmail.com>
2026-04-27 17:56:42 +0530

    Time: 11 ms (77.27%) | Memory: 19.2 MB (99.89%) - LeetSync

--- 87-scramble-string/scramble-string.py
+++ 87-scramble-string/scramble-string.py
@@ -3,11 +3,9 @@ class Solution:
         self.memo = {}
 
     def isScramble(self, s1: str, s2: str) -> bool:
-        # If we've already solved this pair, return the result
         if (s1, s2) in self.memo:
             return self.memo[(s1, s2)]
         
-        # Base cases
         if s1 == s2:
             return True
         if sorted(s1) != sorted(s2): # Pruning: must have same characters
@@ -15,13 +13,11 @@ class Solution:
             
         n = len(s1)
         for i in range(1, n):
-            # Case 1: No swap at this level
             if (self.isScramble(s1[:i], s2[:i]) and 
                 self.isScramble(s1[i:], s2[i:])):
                 self.memo[(s1, s2)] = True
                 return True
             
-            # Case 2: Substrings were swapped at this level
             if (self.isScramble(s1[:i], s2[n-i:]) and 
                 self.isScramble(s1[i:], s2[:n-i])):
                 self.memo[(s1, s2)] = True
